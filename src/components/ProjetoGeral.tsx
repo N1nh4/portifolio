eb3e93d2fd1805f3269c6e87aedfff099c3ce302
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-02-07 15:44:38 -0300

    aplicando responsividade

--- src/components/ProjetoGeral.tsx
+++ src/components/ProjetoGeral.tsx
@@ -14,17 +14,17 @@ interface ProjetoGeralProps {
 const ProjetoGeral: React.FC<ProjetoGeralProps> = ({ titulo, descricao, imagem, categoria, saibaMais}) => {
 
     return (
-        <div className="flex justify-center items-center bg-slate-800 rounded-xl p-2 m-4 h-64 transition-transform duration-300 ease-in-out transform hover:scale-105 z-0">
+        <div className="flex flex-col md:flex-row justify-center items-center bg-slate-800 rounded-xl p-2 m-4 md:h-64 transition-transform duration-300 ease-in-out transform hover:scale-105 z-0">
 
         
-                <div className=" flex flex-col w-96 h-full items-end p-2">
+                <div className="mt-2 md:mt-0 lg:mt-0 flex flex-col md:w-96 lg:w-96 h-full items-end p-2">
                     <div>
                         <Image src={imagem} alt={titulo} width={500} height={300}></Image>
                         <a href={saibaMais} target="_blank"> 
                             <button className="bg-gray-400 w-full">Saiba mais aqui!</button>
                         </a>
                     </div>
-                    <div className="flex gap-3 h-full w-full items-end ">
+                    <div className="mt-4 md:mt-0 lg:mt-0 flex gap-3 h-full w-full items-end ">
                         {categoria.map((id) => {
                             const tecnologia = tecnologias[id];
                             return (
@@ -42,7 +42,7 @@ const ProjetoGeral: React.FC<ProjetoGeralProps> = ({ titulo, descricao, imagem, 
                     </div>
                 </div>
 
-                <div className=" w-64 h-full flex flex-col p-2 ">
+                <div className=" md:w-64 lg:w-64 h-full flex flex-col p-2 ">
                     <div className="bg-white flex flex-1 p-2 rounded-sm mb-2">
                         <p className="text-justify hyphens-auto text-black ">{descricao}</p>
                     </div>
