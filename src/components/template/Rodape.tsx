8c9fabfda58081993f5c3ad28b08350bbd3ed679
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-02-14 13:31:34 -0300

    Update Rodape.tsx

--- src/components/template/Rodape.tsx
+++ src/components/template/Rodape.tsx
@@ -1,13 +1,13 @@
 export default function Rodape() {
-    return(
-        <div className="flex items-center justify-center w-full h-8 mt-auto pb-4">
-            <span>Copyright © 2025 
-                <a href="https://github.com/N1nh4" target="_blank" className="text-blue-500">
-                    Alana Abreu. 
-                </a> 
+    return (
+        <footer className=" bottom-0 left-0 w-full h-8 pb-6 flex items-center justify-center z-50">
+            <p className="text-center text-sm sm:text-base">
+                Copyright © 2025{' '}
+                <a href="https://github.com/N1nh4" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                    Alana Abreu
+                </a>{' '}
                 Todos os direitos reservados.
-            </span>
-        
-        </div>
+            </p>
+        </footer>
     );
 }
