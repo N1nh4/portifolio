019b3f9ff6473d5ecd9a9685ffd9c956f8bb1741
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-02-15 14:17:05 -0300

    footer p

--- src/components/template/Rodape.tsx
+++ src/components/template/Rodape.tsx
@@ -1,6 +1,6 @@
 export default function Rodape() {
     return (
-        <footer className=" bottom-0 left-0 w-full h-8 pb-6 flex items-center justify-center z-50">
+        <footer className=" bottom-0 left-0 w-full h-8 pb-8 flex items-center justify-center z-50">
             <p className="text-center text-xs md:text-base">
                 Copyright © 2025{' '}
                 <a href="https://github.com/N1nh4" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
