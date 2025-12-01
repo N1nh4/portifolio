2103e15fc5a0756d2e6600538eb54eee877b6cf0
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-12-01 14:23:05 -0300

    atualizando curriculo e exeriencias profissionais

--- src/app/Certificados/page.tsx
+++ src/app/Certificados/page.tsx
@@ -2,120 +2,112 @@
 import Cabecalho from "@/components/template/Cabecalho";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 
-
 const projects = [
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/5c7f4533-ec95-44e3-8c7e-1776a229bd23?lang",
-    imagem: "/certificados/react-js.png"
+    imagem: "/certificados/react-js.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/1038885b-42f5-4396-a0b7-414866ac8765?lang",
-    imagem: "/certificados/redes-introducao.png"
+    imagem: "/certificados/redes-introducao.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/77d967e6-8969-487b-8e97-fe79772cef98",
-    imagem: "/certificados/modelagem-bd.png"
+    imagem: "/certificados/modelagem-bd.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/dec81681-54a2-46f7-bf15-d5212e81b2c4",
-    imagem: "/certificados/html-css.png"
+    imagem: "/certificados/html-css.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/510ccae0-b6bc-49dd-b845-d19c7ade8c52",
-    imagem: "/certificados/postgresql.png"
+    imagem: "/certificados/postgresql.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/1ee5dbb7-96fb-423d-9ed2-4aad8928cccb",
-    imagem: "/certificados/sqlite.png"
+    imagem: "/certificados/sqlite.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/d5a16615-f80d-4ee7-8ff7-41dd49cc5370",
-    imagem: "/certificados/php-avancado.png"
+    imagem: "/certificados/php-avancado.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/f17adb57-0e6b-45cd-bb09-baa2163540ee",
-    imagem: "/certificados/php-introducao.png"
+    imagem: "/certificados/php-introducao.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/dda09965-a3cd-43f9-bbf2-cc5e07d0662f",
-    imagem: "/certificados/git-github.png"
+    imagem: "/certificados/git-github.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/e1fabffe-1c4d-402b-89eb-405f5f1cc910",
-    imagem: "/certificados/php-poo.png"
+    imagem: "/certificados/php-poo.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/f97de899-90eb-49c8-8d53-5b0ac6e14a4e",
-    imagem: "/certificados/python.png"
+    imagem: "/certificados/python.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/3da8e9c8-2d27-4839-80b6-30937b874683",
-    imagem: "/certificados/js-html.png"
+    imagem: "/certificados/js-html.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/b5c16e6d-1744-4dfe-98ea-02e945ef9f46",
-    imagem: "/certificados/php-conceitos.png"
+    imagem: "/certificados/php-conceitos.png",
   },
   {
     title: "",
-    description:
-      "",
+    description: "",
     link: "https://cursos.alura.com.br/certificate/e1908762-0ef0-456b-894a-48030edaa981",
-    imagem: "/certificados/sql-mysql.png"
+    imagem: "/certificados/sql-mysql.png",
   },
- 
-  
+  {
+    title: "",
+    description: "",
+    link: "https://www.udemy.com/certificate/UC-7c60a442-cccc-4b69-a9e8-4d71d20aa3a9/",
+    imagem: "/certificados/estrutura-dados-c.png",
+  },
+  {
+    title: "",
+    description: "",
+    link: "https://www.semcomp.com.br/",
+    imagem: "/certificados/semcomp-23.png",
+  },
 ];
 
-
 export default function Certificados() {
-    return (
-        <div className="mt-14 h-[calc(100vh-3rem)]  overflow-y-auto overflow-x-hidden">
-            <Cabecalho />
-            <div className="max-w-7xl mx-auto px-8">
-                <HoverEffect items={projects} />
-            </div>
-        </div>
-
-    )
+  return (
+    <div className="mt-14 h-[calc(100vh-3rem)]  overflow-y-auto overflow-x-hidden">
+      <Cabecalho />
+      <div className="max-w-7xl mx-auto px-8">
+        <HoverEffect items={projects} />
+      </div>
+    </div>
+  );
 }
-
--- src/app/Sobre/page.tsx
+++ src/app/Sobre/page.tsx
@@ -3,106 +3,168 @@ import Rodape from "@/components/template/Rodape";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import { Timeline } from "@/components/ui/timeline";
 
-
-
 export default function Sobre() {
+  const items = [
+    { imgSrc: "/imagens/html.svg", alt: "html" },
+    { imgSrc: "/imagens/css.svg", alt: "css" },
+    { imgSrc: "/imagens/java-script.svg", alt: "js" },
+    { imgSrc: "/imagens/tailwind.svg", alt: "tailwind" },
+    { imgSrc: "/imagens/react.svg", alt: "react" },
+    { imgSrc: "/imagens/python.svg", alt: "python" },
+    { imgSrc: "/imagens/mysql.svg", alt: "mysql" },
+    { imgSrc: "/imagens/next-js.svg", alt: "next" },
+    { imgSrc: "/imagens/springBoot.svg", alt: "spring" },
+    { imgSrc: "/imagens/php.svg", alt: "php" },
+    { imgSrc: "/imagens/laravel.svg", alt: "laravel" },
+  ];
+  return (
+    <div className="mt-14 h-[calc(100vh-3rem)]  overflow-y-auto overflow-x-hidden">
+      <Cabecalho />
+      <div className="  flex flex-col justify-center items-center mt-28 ">
+        <div className=" flex flex-col md:flex-row justify-center items-center">
+          <div className=" w-48 ">
+            <img src="/imagens/perfil.jpeg" alt="perfil" />
+          </div>
+          <div className="w-3/4 md:w-2/5 p-6 flex flex-col">
+            <h1 className="font-bold text-center md:text-start">
+              Alana Carolina Pereira Abreu
+            </h1>
 
-    const items = [
-        { imgSrc: "/imagens/html.svg", alt: "html" },
-        { imgSrc: "/imagens/css.svg", alt: "css" },
-        { imgSrc: "/imagens/java-script.svg", alt: "js" },
-        { imgSrc: "/imagens/tailwind.svg", alt: "tailwind" },
-        { imgSrc: "/imagens/react.svg", alt: "react" },
-        { imgSrc: "/imagens/python.svg", alt: "python" },
-        { imgSrc: "/imagens/mysql.svg", alt: "mysql" },
-        { imgSrc: "/imagens/next-js.svg", alt: "next" },   
-        { imgSrc: "/imagens/springBoot.svg", alt: "spring" },
-        { imgSrc: "/imagens/php.svg", alt: "php" },
-        { imgSrc: "/imagens/laravel.svg", alt: "laravel" },
+            <p className="text-justify mt-3">
+              Desenvolvedora Fullstack, atualmente estudante de Sistemas de
+              Informação na Universidade do Estado da Bahia (UNEB). Experiência
+              com desenvolvimento web utilizando React, Next.js, Spring Boot e
+              Laravel, além de conhecimentos em bancos de dados relacionais e
+              não relacionais. Também já fui bolsista de Iniciação Científica no
+              Grupo de Pesquisa em Bioinformática e Biologia Computacional
+              (G2BC), atuando com análise de dados e desenvolvimento de scripts
+              para apoio à pesquisa científica. Atualmente sou voluntária do
+              projeto web, IaEditais da Fiocruz. Sou uma pessoa comunicativa,
+              determinada e com perfil de liderança.{" "}
+            </p>
 
-    ];
-    return (
-        <div className="mt-14 h-[calc(100vh-3rem)]  overflow-y-auto overflow-x-hidden">
-            <Cabecalho />
-            <div className="  flex flex-col justify-center items-center mt-28 ">
-                <div className=" flex flex-col md:flex-row justify-center items-center">
-                    <div className=" w-48 ">
-                        <img src="/imagens/perfil.jpeg" alt="perfil" />
-                    </div>
-                    <div className="w-3/4 md:w-2/5 p-6 flex flex-col">
-                        <h1 className="font-bold text-center md:text-start">Alana Carolina Pereira Abreu</h1>
-
-                        <p className="text-justify mt-3">Desenvolvedora Fullstack, atualmente estudante de Sistemas de Informação na Universidade do Estado da Bahia (UNEB). Experiência com desenvolvimento web utilizando React, Next.js, Spring Boot e Laravel, além de conhecimentos em bancos de dados relacionais e não relacionais. Também já fui bolsista de Iniciação Científica no Grupo de Pesquisa em Bioinformática e Biologia Computacional (G2BC), atuando com análise de dados e desenvolvimento de scripts para apoio à pesquisa científica. Sou uma pessoa comunicativa, determinada e com perfil de liderança. </p>
-                        
-                        <br />
-                      
-                       
-                    </div>
-                </div>
-
-                <h2 className="mt-16 font-bold">Principais Ferramentas</h2>
-                <InfiniteMovingCards items={items} direction="left" speed="fast" pauseOnHover={true} />
-
-                <div className=" lg:w-3/4 p-6 flex flex-col">
-                    <h2 className=" mt-6 text-center text-2xl md:text-5xl font-mono">Experiência Profissional</h2>
-                <Timeline 
-                    data={[
-                        {
-                            title: "Horizon Inovação e Tecnologia - Estágio em desenvolvimento fullstack",
-                            content: (
-                                <div>
-                                <h1 className="font-bold">AGO 2025 - SET 2025</h1> <br />
-                                <p className="text-lg text-justify">Atuei na manutenção e evolução de sistemas web, desenvolvendo funcionalidades, interfaces e módulos com Laravel (PHP), React, Vue.js e TypeScript. Trabalhei com PostgreSQL, MySQL e Supabase, realizando ajustes e integrações. Participei de projetos ágeis, utilizando Monday para gestão das tasks, além de atuar na correção de bugs, implementação de testes, uso de Docker em servidores Linux e versionamento de código com Git. Também tive contato com ferramentas de suporte à produtividade e inteligência artificial, como Cursor e Lovable, para otimizar partes do processo de desenvolvimento.</p>
-                                </div>
-                            )
-
-                        },
-                        {
-                            title: "G2BC - Bolsista em Iniciação Científica",
-                            content: (
-                                <div>
-                                <h1 className="font-bold">SET 2024 - SET 2025</h1> <br />
-                                <p className="text-lg text-justify">Pesquisa interdisciplinar analisando dados genômicos, epidemiológicos e climáticos do vírus da dengue (DENV) na Bahia, focando em padrões de incidência e fatores de risco. As atividades incluem coleta, organização e análise de dados com modelos de séries temporais, reforçando habilidades em ciência de dados, estatística e programação, integrando Sistemas de Informação à bioinformática e saúde pública.</p>
-                                </div>
-                            ),
-                        },
-                        {
-                            title: "L&A SERVIÇOS - Estágio em administração",
-                            content: (
-                                <div>
-                                <h1 className="font-bold">JUL 2022 - OUT 2023</h1> <br />
-                                <p className="text-lg text-justify">Responsável pela documentação das rotinas e colaboração com a equipe nos setores de faturamento, financeiro, ouvidoria, recursos humanos e gerenciais. As principais atividades incluíam organização de documentos para faturamento, emissão de GRUs, boletos e CNVs, cobrança, suporte aos inquilinos, elaboração de contratos, lançamentos financeiros, organização de relatórios SEFIP e criação de planilhas. </p>
-                                </div>
-                            ),
-                        },  
-                        
-                        
-                    ]}
-                />
-                    
-                </div>
-              
-                <h2 className="font-bold mt-8">Redes Sociais</h2>
-                <div className="flex flex-1 gap-5 m-4 mb-14">
-                    <a href="https://www.linkedin.com/in/alana-abreu/" target="_blank">
-                    <img src="/imagens/linkedin.svg" alt="linkedin" className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"/>
-                    </a>
-                    <a href="https://wa.me/5571986498698" target="_blank">
-                        <img src="/imagens/whatsapp.svg" alt="whatsapp" className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"/>
-                    </a>
-                    <a href="mailto:alanaabreu1103@gmail.com" target="_blank">
-                        <img src="/imagens/gmail-novo.svg" alt="gmail" className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"/>
-                    </a>
-                    <a href="https://github.com/N1nh4" target="_blank">
-                        <img src="/imagens/github.svg" alt="github" className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"/>
-                    </a>
-                    <a href="https://www.instagram.com/alana__abreu/" target="_blank">
-                        <img src="/imagens/instagram.svg" alt="nextjs" className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"/>
-                    </a>
-                </div>
-            </div>
-            <Rodape />
+            <br />
+          </div>
         </div>
-        
-    )
+
+        <h2 className="mt-16 font-bold">Principais Ferramentas</h2>
+        <InfiniteMovingCards
+          items={items}
+          direction="left"
+          speed="fast"
+          pauseOnHover={true}
+        />
+
+        <div className=" lg:w-3/4 p-6 flex flex-col">
+          <h2 className=" mt-6 text-center text-2xl md:text-5xl font-mono">
+            Experiência Profissional
+          </h2>
+          <Timeline
+            data={[
+              {
+                title:
+                  "Fiocruz projeto IaEditais - Voluntária em Desenvolvimento WebFullStack",
+                content: (
+                  <div>
+                    <h1 className="font-bold">MAIO 2025 - ATUALMENTE</h1> <br />
+                    <p className="text-lg text-justify">
+                      Atuo como desenvolvedora fullstack voluntária no projeto
+                      IaEditais, iniciativa da Fiocruz. No frontend, implemento
+                      módulos usando React, Next.js, TypeScript e Tailwind,
+                      seguindo boas práticas e padrões de arquitetura. No
+                      backend, contribuo em funcionalidades desenvolvidas com
+                      FastAPI, SQLAlchemy e APIs REST, auxiliando na modelagem
+                      de dados e integrações. Também participei da prototipação
+                      de alta fidelidade no Figma e utilizo Git/GitHub no fluxo
+                      colaborativo do projeto.{" "}
+                    </p>
+                  </div>
+                ),
+              },
+              {
+                title:
+                  "Horizon Inovação e Tecnologia - Estágio em desenvolvimento fullstack",
+                content: (
+                  <div>
+                    <h1 className="font-bold">AGO 2025 - SET 2025</h1> <br />
+                    <p className="text-lg text-justify">
+                      Atuei na manutenção e evolução de sistemas web,
+                      desenvolvendo funcionalidades, interfaces e módulos com
+                      Laravel (PHP), React, Vue.js e TypeScript. Trabalhei com
+                      PostgreSQL, MySQL e Supabase, realizando ajustes e
+                      integrações. Participei de projetos ágeis, utilizando
+                      Monday para gestão das tasks, além de atuar na correção de
+                      bugs, implementação de testes, uso de Docker em servidores
+                      Linux e versionamento de código com Git. Também tive
+                      contato com ferramentas de suporte à produtividade e
+                      inteligência artificial, como Cursor e Lovable, para
+                      otimizar partes do processo de desenvolvimento.
+                    </p>
+                  </div>
+                ),
+              },
+              {
+                title: "G2BC - Bolsista em Iniciação Científica",
+                content: (
+                  <div>
+                    <h1 className="font-bold">SET 2024 - SET 2025</h1> <br />
+                    <p className="text-lg text-justify">
+                      Pesquisa interdisciplinar analisando dados genômicos,
+                      epidemiológicos e climáticos do vírus da dengue (DENV) na
+                      Bahia, focando em padrões de incidência e fatores de
+                      risco. As atividades incluem coleta, organização e análise
+                      de dados com modelos de séries temporais, reforçando
+                      habilidades em ciência de dados, estatística e
+                      programação, integrando Sistemas de Informação à
+                      bioinformática e saúde pública.
+                    </p>
+                  </div>
+                ),
+              },
+            ]}
+          />
+        </div>
+
+        <h2 className="font-bold mt-8">Redes Sociais</h2>
+        <div className="flex flex-1 gap-5 m-4 mb-14">
+          <a href="https://www.linkedin.com/in/alana-abreu/" target="_blank">
+            <img
+              src="/imagens/linkedin.svg"
+              alt="linkedin"
+              className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"
+            />
+          </a>
+          <a href="https://wa.me/5571986498698" target="_blank">
+            <img
+              src="/imagens/whatsapp.svg"
+              alt="whatsapp"
+              className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"
+            />
+          </a>
+          <a href="mailto:alanaabreu1103@gmail.com" target="_blank">
+            <img
+              src="/imagens/gmail-novo.svg"
+              alt="gmail"
+              className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"
+            />
+          </a>
+          <a href="https://github.com/N1nh4" target="_blank">
+            <img
+              src="/imagens/github.svg"
+              alt="github"
+              className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"
+            />
+          </a>
+          <a href="https://www.instagram.com/alana__abreu/" target="_blank">
+            <img
+              src="/imagens/instagram.svg"
+              alt="nextjs"
+              className="md:h-14 md:w-14 transition-transform duration-300 ease-in-out transform hover:scale-125"
+            />
+          </a>
+        </div>
+      </div>
+      <Rodape />
+    </div>
+  );
 }
